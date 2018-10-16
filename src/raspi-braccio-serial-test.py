dda98956b8ad730fd502aa494957dd7b7c0f1af9
John J Hritz <john-j-hritz@sbcglobal.net>
2018-10-16 11:41:04 -0700

    Initial commit

---
+++ src/raspi-braccio-serial-test.py
@@ -0,0 +1,10 @@
+"""
+Module: raspi-braccio-serial-test
+Project: raspi-braccio-serial-test
+Description: Test the response of a Tinkertoy Braccio robot arm controlled from a Raspberry Pi via USB connection.
+Assumes commands are compliant with the sketch in this repo: https://github.com/jjhritz/BraccioSerialControlTest
+
+Date: 10/16/2018
+Author: John J. Hritz
+Email: john-j-hritz@sbcglobal.net
+"""
